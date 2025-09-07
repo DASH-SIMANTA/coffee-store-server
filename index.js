c5196fed1d95bed9514e9a6aa26adf1e4efbf0bb
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-09-07 16:25:33 +0600

    user api

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const coffeeCollection = client.db('coffeeDB').collection('coffee');
+        const userCollection = client.db('coffeeDB').collection('users');
 
         // Read (GET) all coffee
         app.get('/coffee', async (req, res) => {
@@ -83,8 +84,47 @@ async function run() {
         })      
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        //user related API
+        
+        app.get('/users', async(req, res)=>{
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);   
+        })
+        app.post('/users', async(req, res) => {
+            const newUser = req.body;
+            console.log('creating new user',newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+         
+        })
+
+        app.patch('/users', async(req, res) => {
+            const email = req.body.email;
+            const filter = {email};
+            // const updatedUser = req.body?.lastSignInTime;
+            // console.log(updatedUser);
+            const updateDoc = {
+                $set: {
+                    lastSignInTime: req.body?.lastSignInTime
+                },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
